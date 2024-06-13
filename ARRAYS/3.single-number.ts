e48de220398b32a123201bfa3080272e472f4f07
Pursotam <rpursotam99@gmail.com>
2024-06-13 17:24:55 +0530

    array: single number in an array solution

--- ARRAYS/3.single-number.ts
+++ ARRAYS/3.single-number.ts
@@ -1,1 +1,12 @@
 // https://leetcode.com/problems/single-number/description/
+function singleNumber(nums: number[]): number {
+  let xor = 0;
+  const n = nums.length;
+
+  for (let i = 0; i < n; ++i) {
+    xor ^= nums[i];
+  }
+  return xor;
+}
+
+// just using the same xor concept that if we xor every value inside the array , the values which comes twice will cancel each other leaving behind the single element.
