4814539b90ad81953f95c725433eee5be560c150
pursotam <rpursotam99@gmail.com>
2024-07-01 11:36:51 +0530

    updated ans for 3 in arrays and hashing

--- 1.ARRAYS-HASHING/3.group-anagrams.ts
+++ 1.ARRAYS-HASHING/3.group-anagrams.ts
@@ -17,3 +17,35 @@ function groupAnagrams(strs: string[]): string[][] {
 
   return [...map.values()];
 }
+
+// * ----------------------------------------------------------------------------------------------
+
+function groupAnagramsOptimalSolution(strs: string[]): string[][] {
+  // handle base case
+  // strs has single element
+  const n = strs.length;
+  if (n == 1) return [strs];
+
+  const map = new Map();
+
+  for (let i = 0; i < n; ++i) {
+    const array = new Array(26).fill(0);
+    for (let j = 0; j < strs[i].length; ++j) {
+      array[strs[i].charCodeAt(j) - 97]++;
+    }
+
+    let key = '';
+    for (let ele of array) {
+      // we added # becase [1,0,10] will be same as [10,1,0] while concatinating and making key
+      key += '#' + ele;
+    }
+
+    if (map.get(key)) {
+      map.get(key).push(strs[i]);
+    } else {
+      map.set(key, [strs[i]]);
+    }
+  }
+
+  return [...map.values()];
+}
